8bb5f2bd889e2f5495cd0b9e0bb442083540f2da
James Lavin <jblavin12@gmail.com>
2023-09-25 13:50:17 -0400

    pushing Brewery List Details and Service starter code

---
+++ vue/src/components/BreweryDetails.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr>
+        <td class="brewery-name">{{ brewery.name }}</td>
+        <td class="contact-info">{{ brewery.contact }}</td>
+        <td class="brewery-history">{{ brewery.history }}</td>
+        <td class="brewery-hours">{{ brewery.hours }}</td>
+        <td class="brewery-address">{{ brewery.address }}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+    name: 'brewery-details',
+    data() {
+        return {
+            brewery: {
+            name: "",
+            contact: "",
+            history: "",
+            hours: "",
+            address: ""
+      }
+    };
+  }
+};
+</script>
---
+++ vue/src/components/BreweryList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <h1>List of all breweries</h1>
+      
+      <div class="individual" v-for="brewery in breweries" v-bind:key="brewery.breweryId">
+        <h2>{{brewery.name}} (ID: {{brewery.breweryId}})</h2>
+        Contact Info: {{brewery.contact}} <br />
+        History: {{brewery.history}} <br />
+        Operating Hours: {{brewery.hours}} <br />
+        Address: {{brewery.address}}
+      </div>
+  </div>
+</template>
+
+<script>
+import breweryService from '../services/BreweryService.js';
+
+export default {
+    name: "BreweryList",
+    data() {
+        return{
+            breweries: []
+        }
+    },
+    // We want to load breweries as soon as someone visits the page
+    created(){
+        breweryService.getBreweries()
+            .then(response => this.breweries = response.data)
+    }
+}
+</script>
+
+<style>
+</style>
