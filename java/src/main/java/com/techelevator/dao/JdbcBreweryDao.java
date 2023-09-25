281013c9b161fd071459dcb0c0e0684af1a406d2
Emily Quiles <emilyquiles1@gmail.com>
2023-09-25 13:37:07 -0400

    Implemented mvc structure

---
+++ java/src/main/java/com/techelevator/dao/JdbcBreweryDao.java
@@ -0,0 +1,114 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Brewery;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JdbcBreweryDao implements BreweryDao{
+
+    private final JdbcTemplate jdbcTemplate;
+    public JdbcBreweryDao(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+    @Override
+    public List<Brewery> getAllBreweries() {
+        //sql query to select breweries
+        String sql = "SELECT brewery_id, brewery_name, contact_info, brewery_history, operating_hours, brewery_img_url, brewery_address FROM breweries";
+        //where breweries get stored
+        List<Brewery> results = new ArrayList<>();
+        try{
+            //send query
+            SqlRowSet queryResults = jdbcTemplate.queryForRowSet(sql);
+            while(queryResults.next()){
+                //call helper method to map results
+                Brewery currentBrewery = mapBrewery(queryResults);
+                //add brewery to list
+                results.add(currentBrewery);
+            }
+        }catch (Exception e){
+            System.out.println("Error occurred when connecting to the database. Exception is: ");
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    @Override
+    public Brewery getBrewery(int brewery_id) {
+        Brewery brewery = null;
+        // sql query to get a brewery by id
+        String sql = "SELECT brewery_id, brewery_name, contact_info, brewery_history, operating_hours, brewery_img_url, brewery_address FROM breweries WHERE brewery_id = ?";
+        try {
+            // send query
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, brewery_id);
+            if (results.next()) {
+                brewery = mapBrewery(results);
+            }
+        } catch (Exception e) {
+            System.out.println("Error occurred when connecting to the database. Exception is: ");
+            e.printStackTrace();
+        }
+        return brewery;
+    }
+
+    @Override
+    public Brewery addBrewery(Brewery newBrewery) {
+        String sql = "INSERT INTO breweries (brewery_name, contact_info, brewery_history, operating_hours, brewery_img_url, brewery_address) VALUES (?, ?, ?, ?, ?, ?)";
+        try {
+            int newBreweryId = jdbcTemplate.queryForObject(sql, int.class,
+                    newBrewery.getBrewery_name(), newBrewery.getContact_info(),newBrewery.getBrewery_history(), newBrewery.getOperating_hours(), newBrewery.getBrewery_img_url(), newBrewery.getBrewery_address());
+            newBrewery = getBrewery(newBreweryId);
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DataAccessException("Unable to connect to server or database", e) {
+            };
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Data integrity violation", e);
+        }
+        return newBrewery;
+    }
+
+
+    @Override
+    public Brewery updateBrewery(Brewery updatedBrewery) {
+        Brewery brewery = null;
+        String sql = "UPDATE breweries SET brewery_name = ?, contact_info = ?, brewery_history = ?, operating_hours = ?, brewery_img_url = ?, brewery_address = ? WHERE brewery_id = ?";
+        try {
+            int numberOfRows = jdbcTemplate.update(sql, brewery.getBrewery_name(), brewery.getContact_info(), brewery.getBrewery_history(), brewery.getOperating_hours(), brewery.getBrewery_img_url(), brewery.getBrewery_address());
+
+            if (numberOfRows == 0) {
+                throw new DataAccessException("Zero rows affected, expected at least one") {
+                };
+            } else {
+                updatedBrewery = getBrewery(brewery.getBrewery_id());
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DataAccessException("Unable to connect to server or database", e) {
+            };
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolationException("Data integrity violation", e);
+        }
+        return updatedBrewery;
+    }
+
+    private Brewery mapBrewery(SqlRowSet row){
+        Brewery brewery = new Brewery();
+        int brewery_id = row.getInt("brewery_id");
+        String brewery_name = row.getString("brewery_name");
+        String contact_info = row.getString("contact_info");
+        String brewery_history = row.getString("brewery_history");
+        String operating_hours = row.getString("operating_hours");
+        String brewery_img_url = row.getString("brewery_img_url");
+        String brewery_address = row.getString("brewery_address");
+
+        brewery = new Brewery(brewery_id, brewery_name, contact_info, brewery_history, operating_hours, brewery_img_url, brewery_address);
+        return brewery;
+    }
+}
---
+++ java/src/main/java/com/techelevator/model/Brewery.java
@@ -0,0 +1,134 @@
+package com.techelevator.model;
+
+import java.util.Objects;
+
+public class Brewery {
+
+    //these are the 5 required properties to show when a user views brewery info, plus an id
+
+    private int brewery_id;
+    private String brewery_name;
+
+    private String contact_info;
+
+    private String brewery_history;
+
+    private String operating_hours;
+
+    private String brewery_img_url;
+
+    private String brewery_address;
+
+    // getters and setters for each
+
+    public int getBrewery_id() {
+        return brewery_id;
+    }
+
+    public void setBrewery_id(int brewery_id) {
+        this.brewery_id = brewery_id;
+    }
+
+    public String getBrewery_name() {
+        return brewery_name;
+    }
+
+    public void setBrewery_name(String brewery_name) {
+        this.brewery_name = brewery_name;
+    }
+
+    public String getContact_info() {
+        return contact_info;
+    }
+
+    public void setContact_info(String contact_info) {
+        this.contact_info = contact_info;
+    }
+
+    public String getBrewery_history() {
+        return brewery_history;
+    }
+
+    public void setBrewery_history(String brewery_history) {
+        this.brewery_history = brewery_history;
+    }
+
+    public String getOperating_hours() {
+        return operating_hours;
+    }
+
+    public void setOperating_hours(String operating_hours) {
+        this.operating_hours = operating_hours;
+    }
+
+    public String getBrewery_img_url() {
+        return brewery_img_url;
+    }
+
+    public void setBrewery_img_url(String brewery_img_url) {
+        this.brewery_img_url = brewery_img_url;
+    }
+
+    public String getBrewery_address() {
+        return brewery_address;
+    }
+
+    public void setBrewery_address(String brewery_address) {
+        this.brewery_address = brewery_address;
+    }
+
+    //constructor with all properties
+    public Brewery(int brewery_id, String brewery_name, String contact_info, String brewery_history, String operating_hours, String brewery_img_url, String brewery_address) {
+        this.brewery_id = brewery_id;
+        this.brewery_name = brewery_name;
+        this.contact_info = contact_info;
+        this.brewery_history = brewery_history;
+        this.operating_hours = operating_hours;
+        this.brewery_img_url = brewery_img_url;
+        this.brewery_address = brewery_address;
+    }
+
+    // constructor with all properties minus the id
+    public Brewery(String brewery_name, String contact_info, String brewery_history, String operating_hours, String brewery_img_url, String brewery_address) {
+        this.brewery_name = brewery_name;
+        this.contact_info = contact_info;
+        this.brewery_history = brewery_history;
+        this.operating_hours = operating_hours;
+        this.brewery_img_url = brewery_img_url;
+        this.brewery_address = brewery_address;
+    }
+
+    // empty constructor
+    public Brewery() {
+    }
+
+    // to string
+
+    @Override
+    public String toString() {
+        return "Brewery{" +
+                "brewery_id=" + brewery_id +
+                ", brewery_name='" + brewery_name + '\'' +
+                ", contact_info='" + contact_info + '\'' +
+                ", brewery_history='" + brewery_history + '\'' +
+                ", operating_hours='" + operating_hours + '\'' +
+                ", brewery_img_url='" + brewery_img_url + '\'' +
+                ", brewery_address='" + brewery_address + '\'' +
+                '}';
+    }
+
+    // equals boolean compares if two breweries are the same
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Brewery brewery = (Brewery) o;
+        return contact_info.equals(brewery.contact_info) && brewery_history.equals(brewery.brewery_history) && operating_hours.equals(brewery.operating_hours) && brewery_img_url.equals(brewery.brewery_img_url) && brewery_address.equals(brewery.brewery_address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(contact_info, brewery_history, operating_hours, brewery_img_url, brewery_address);
+    }
+}
