6d5716a36c0db3a0d6c73b7af76daf21a467e2ad
Connor Finnegan <connorfinneganb@gmail.com>
2023-09-25 17:56:00 -0500

    nearly working brewery list

--- vue/src/services/BreweryService.js
+++ vue/src/services/BreweryService.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const http = axios.create({
-    baseURL: ""
-});
-
 export default {
     
     getBreweries() {
-        return http.get('/allBreweries')
-    }
+        return axios.get('/allBreweries');
+    },
+
+    
 }
