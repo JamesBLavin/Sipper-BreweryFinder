77ff1b925da304b1b91cf6561553a18bc7b1d1d8
Michael <michael.verlander10@gmail.com>
2023-09-25 13:47:22 -0400

    Pushing data tables

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS breweries;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +9,29 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE breweries (
+    brewery_id SERIAL,
+    brewery_name VARCHAR(50) NOT NULL,
+    contact_info VARCHAR(500),
+    brewery_history VARCHAR(500),
+    operating_hours VARCHAR(300),
+    brewery_img_url VARCHAR(500),
+    brewery_address VARCHAR(500),
+    CONSTRAINT PK_brewery PRIMARY KEY (brewery_id)
+);
+
+CREATE TABLE beers (
+   beer_id SERIAL,
+   brewery_id INT,
+   beer_name VARCHAR(50) NOT NULL UNIQUE,
+   beer_description VARCHAR(500),
+   abv NUMERIC(2,1),
+   ibu INT,
+   beer_rating INT,
+   beer_review VARCHAR(500),
+   CONSTRAINT PK_beer PRIMARY KEY (beer_id)
 );
 
 COMMIT TRANSACTION;
