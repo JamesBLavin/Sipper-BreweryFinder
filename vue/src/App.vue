c73501d83aeadf766589d510f2b71cae4bf45762
Connor Finnegan <connorfinneganb@gmail.com>
2023-09-27 23:57:48 -0500

    updated styling for PO, routes for routerlinks, revamped home page, and fixed bootstrap

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div :id="navHome ? 'nav-home' : ''" class="all-nav">
       <div id="logo-name">
      <img src="./assets/clink.png" alt="testing" id="main-logo"/>&nbsp;
-     <h1>sipper</h1>
+     <h1 id="title">sipper</h1>
      </div>
      <div id="links">
-      <router-link :to="{ name: 'home' }" tag="h2" class="nav-links">home</router-link>
-      <router-link :to="{ name: 'breweries' }" tag="h2" class="nav-links">breweries</router-link>
-      <router-link :to="{ name: 'beers' }" tag="h2" class="nav-links">beers</router-link>
-      <router-link :to="{ name: 'profile' }" v-if="$store.state.token != ''" tag="h2" class="nav-links" id="profile">{{ this.$store.state.user.username }}</router-link>
-      <router-link :to="{ name: 'add-brewery' }" tag="h2" class="nav-links" v-show="this.$store.state.user.authorities && this.$store.state.user.authorities[0].name == 'ROLE_ADMIN'">+ brewery</router-link>
-      <router-link :to="{ name: 'logout' }" v-if="$store.state.token != ''" tag="h2" class="nav-links">logout</router-link>
+      <router-link :to="{ name: 'home' }" tag="h5" class="nav-links">home</router-link>
+      <router-link :to="{ name: 'beers' }" tag="h5" class="nav-links">beers</router-link>
+      <router-link :to="{ name: 'profile' }" v-if="$store.state.token != ''" tag="h5" class="nav-links" id="profile">{{ this.$store.state.user.username }}</router-link>
+      <router-link :to="{ name: 'add-brewery' }" tag="h5" class="nav-links" v-show="this.$store.state.user.authorities && this.$store.state.user.authorities[0].name == 'ROLE_ADMIN'">+ brewery</router-link>
+      <router-link :to="{ name: 'login' }" v-if="loggedIn" tag="h5" class="nav-links">login</router-link>
+      <router-link :to="{ name: 'logout' }" v-if="!loggedIn" tag="h5" class="nav-links">logout</router-link>
       </div>
     </div>
     <router-view id="view"/>
@@ -27,15 +27,23 @@
 <script>
 
 export default {
-  name: 'app'
+  name: 'app',
+  computed: {
+    loggedIn() {
+      return this.$store.state.token == '';
+    },
+    navHome() {
+      return this.$route.path == '/';
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 @import "~@/assets/scss/vendors/bootstrap-vue/index";
 
-body {
-  background: #f0ebdf;
+#title {
+  font-size: 650%; 
 }
 
 #app {
@@ -43,7 +51,7 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: orangered;
+  color: black;
   margin: 25px;
   font-family: 'Open Sans', sans-serif;
 font-family: 'Roboto', sans-serif;
@@ -54,23 +62,36 @@ font-family: 'Roboto Flex', sans-serif;
   width: 10%;
   margin: 10px;
   min-width: 100px;
+  
 }
 
+.all-nav {
+  color: white;
+}
 
-#nav {
+#nav-home {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-areas: "namelogo links";
-  padding-bottom: 5px;
-  background: wheat;
+  padding-bottom: 50vw;
   border-radius: 25px;
   margin-bottom: 10px;
   margin-top: 25px;
+  background-image: url('./assets/52475430061_6b2df9ebae_b.jpg');
+  background-size: 100vw;
+  max-width: 100vw;
+  background-repeat: no-repeat;
 }
+
+
 
 #logo-name {
   grid-area: namelogo;
   display: flex;
+  background: orangered;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 15px;
 }
 
 #links {
@@ -78,6 +99,8 @@ font-family: 'Roboto Flex', sans-serif;
   margin-top: 50px;
   display: flex;
   justify-content: space-around;
+  align-items: flex-end;
+  margin-bottom: 7px;
 }
 
 #nav h1 {
@@ -97,6 +120,14 @@ font-family: 'Roboto Flex', sans-serif;
 
 h1, h2, h3, h4, h5, h6 {
   font-weight: 900;
+}
+
+.nav-links {
+  background: orangered;
+  border-radius: 10px;
+  padding: 5px 20px 5px 20px;
+  height: 30%;
+  ;
 }
 
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,11 +1,90 @@
 <template>
   <div class="home">
-    <h1>thanks for drinking with us, {{ this.$store.state.user.username }}!</h1>
+    <div class="search-container">
+      <form class="search-bar" @submit.prevent="toggleShowBr">
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div>
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Dropdown button
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
+        <button>Save</button>
+      </form>
+    </div>
+    <h1 v-show="$store.state.token != ''">
+      thanks for drinking with us, {{ this.$store.state.user.username }}!
+    </h1>
+    <breweries v-show="this.showBr"/>
   </div>
 </template>
 
 <script>
+import Breweries from "./Breweries.vue";
 export default {
-  name: "home"
+  components: { Breweries },
+  name: "home",
+  data() {
+    return {
+      showBr: false
+    }
+  },
+  methods: {
+    toggleShowBr() {
+      this.showBr = !this.showBr;
+      return this.showBr;
+    }
+  }
 };
 </script>
+
+<style scoped>
+h1 {
+  color: black;
+}
+
+.search-bar {
+  /* margin-top: -25%;
+  margin-bottom: 25%;
+  display: flex;
+  justify-content: center;
+  padding-top: 1%;
+  padding-left: -20%;
+  padding-right: -20%;
+  margin-left: 33%;
+  justify-content: center; */
+  border: black solid 3px;
+  border-radius: 10px;
+  margin-top: -60%;
+  margin-bottom: 25%;
+  padding: 10px;
+  background: white;
+  display: flex;
+  justify-content: center;
+}
+
+.search-container {
+  display: flex;
+  margin-left: 40%;
+}
+
+
+</style>
