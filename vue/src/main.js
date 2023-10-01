b2b1f2abc735cd7265a07fd8ebb863535ef54bc1
James Lavin <jblavin12@gmail.com>
2023-09-30 21:06:24 -0400

    removed font package

--- vue/src/main.js
+++ vue/src/main.js
@@ -7,20 +7,6 @@ import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-/* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-/* add icons to the library */
-library.add(faUserSecret)
-
-/* add font awesome icon component */
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
