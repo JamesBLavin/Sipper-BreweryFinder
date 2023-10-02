6f941efa5b1c211d177fbfb542bcc6c243baaf66
Emily Quiles <emilyquiles1@gmail.com>
2023-10-02 15:02:02 -0400

    worked on reviews

---
+++ vue/src/views/Reviews.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+<review-card :review="review" v-for="review in reviews" :key="review.review_id"> </review-card>
+
+  </div>
+</template>
+
+<script>
+import ReviewService from '../services/ReviewService'
+import ReviewCard from '../components/ReviewCard.vue'
+export default {
+  components: { ReviewCard }, 
+  data(){
+     return{ 
+         reviews:[]
+      }
+      
+  },
+  created(){
+    ReviewService.getAllReviews().then(resp =>
+    {this.reviews = resp.data;});
+  }
+  
+
+
+}
+</script>
+
+<style>
+
+</style>
